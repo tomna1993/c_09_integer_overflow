b09d701031cd12e4f78642a85f7a51a62ccedafc
TomasNagy <tomi.nagy@outlook.com>
2023-05-06 20:01:08 +0200

    Add comments

--- overflow.c
+++ overflow.c
@@ -4,8 +4,13 @@
 int main(void)
 {
 	// Integer numbers would overflow if user input numbers bigger than 2 000 000 000
+	// int a = get_int("a= ");
+	// int b = get_int("b= ");
+
+	// printf("a + b = %i", a + b); // i = 32bit = -2 147 483 648 to 2 147 483 647
+
 	long a = get_long("a= ");
 	long b = get_long("b= ");
 
-	printf("a + b = %lld", a + b); // lld = long long decimal = 64bit
+	printf("a + b = %lld", a + b); // lld = long long decimal = 64bit = -9 223 372 036 854 775 807 to 9 223 372 036 854 775 806
 }
