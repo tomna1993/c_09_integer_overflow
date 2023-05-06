e033e31467783f7378955c3184b6a77984825b22
TomasNagy <tomi.nagy@outlook.com>
2023-05-06 19:47:39 +0200

    Include the correct cs50 library

--- overflow.c
+++ overflow.c
@@ -1,9 +1,9 @@
+#include <cs50.h>
 #include <stdio.h>
-#include <cs50.c>
 
 int main(void)
 {
-	// Integer overflow
+	// Integer numbers would overflow if user input numbers bigger than 2 000 000 000
 	long a = get_long("a= ");
 	long b = get_long("b= ");
 
